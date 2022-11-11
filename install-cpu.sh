1885658c551e74f364e94e5eaf039b75c37c7281
Bas <55401336+BasToTheMax@users.noreply.github.com>
2022-11-11 08:05:23 +0100

    Create install-cpu.sh

---
+++ install-cpu.sh
@@ -0,0 +1,20 @@
+apt-get update -y
+apt install git python3 python3-pip nginx -y
+
+mkdir /root/AI
+cd /root/AI
+git clone https://github.com/BasToTheMax/BTTM-AI .
+
+rm /etc/ngix/sites-enabled/default
+cp nginx-site.conf /etc/ngix/sites-enabled/default
+
+servixe nginx restart
+
+pip3 install Pillow diffusers accelerate transformers gradio
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
+
+huggingface-cli login
+git config --global credential.helper store
+
+cp bttm.service /etc/systemd/system/bttm.service
+service bttm stop
