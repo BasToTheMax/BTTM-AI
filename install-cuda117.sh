972b47f863286de6fe8b27cb28fe8333b3eea70c
Bas <55401336+BasToTheMax@users.noreply.github.com>
2022-11-15 10:37:55 +0100

    Update install-cuda117.sh

--- install-cuda117.sh
+++ install-cuda117.sh
@@ -5,16 +5,17 @@ mkdir /root/AI
 cd /root/AI
 git clone https://github.com/BasToTheMax/BTTM-AI .
 
-rm /etc/ngix/sites-enabled/default
-cp nginx-site.conf /etc/ngix/sites-enabled/default
+rm /etc/nginx/sites-enabled/default
+cp nginx-site.conf /etc/nginx/sites-enabled/default
 
 servixe nginx restart
 
 pip3 install Pillow diffusers accelerate transformers gradio
 pip3 install torch torchvision torchaudio
 
-huggingface-cli login
-git config --global credential.helper store
-
 cp bttm.service /etc/systemd/system/bttm.service
+systemctl enable bttm --now
 service bttm stop
+
+git config --global credential.helper store
+huggingface-cli login
